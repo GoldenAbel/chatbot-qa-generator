8eef47cde6152d9f884dbc6865155b6900724b57
shuoh <hanshuo.cmu@gmail.com>
2016-04-20 10:50:09 -0700

    Generate QA pairs in JSON

--- bin/generate_qa_pairs.py
+++ bin/generate_qa_pairs.py
@@ -1,3 +1,4 @@
+import json
 from qagen.data_provider.knowledge_data_providers import *
 from qagen.qa.collector import KnowledgeDataQaPairCollector
 from qagen.qa.generators import *
@@ -13,8 +14,19 @@ qa_collector.register_generator(Investor, InvestorQaGenerator(data_provider))
 qa_collector.register_generator(Job, JobQaGenerator(data_provider))
 
 qa_collector.collect_from(data_provider)
+print '%d QA pairs collected.' % len(qa_collector.qa_pairs)
 
-for qa_pair in qa_collector.qa_pairs:
-    print qa_pair
-print '%d QA pairs collected!' % len(qa_collector.qa_pairs)
+is_for_training = True
+
+if is_for_training:
+    print 'Adding additional random answers for rank labeling...'
+    output_path = 'qa_pairs_labeled.json'
+else:
+    output_path = 'qa_pairs_unlabeled.json'
+
+qa_pairs_json_data = [qa_pair.to_json_dict(is_for_training=is_for_training) for qa_pair in qa_collector.qa_pairs]
+qa_json_str = json.dumps(qa_pairs_json_data, indent=2, sort_keys=True)
+with open(output_path, 'w') as output_file:
+    output_file.write(qa_json_str)
+print 'All QA pairs dumped at ' + output_path
 
--- qagen/qa/qa_pair.py
+++ qagen/qa/qa_pair.py
@@ -4,8 +4,31 @@ class QAPair(object):
         self.question = question
         self.answer = answer
         self.context_map = context_map
+        self.ranked_answers = [{
+            'answer': answer,
+            'rank': 1
+        }]
 
     def __repr__(self):
         return 'Q: %s, A: %s' % (self.question, self.answer)
 
+    def add_ranked_answer(self, answer, rank):
+        self.ranked_answers.append({
+            'answer': answer,
+            'rank': rank
+        })
+
+    def to_json_dict(self, is_for_training=False):
+        result = {
+            'question': self.question,
+            'answer': self.answer,
+            'context': self.context_map,
+        }
+        if is_for_training:
+            result.update({
+                'question_topic_words': [],
+                'answer_topic_words': [],
+                'ranked_answers': self.ranked_answers
+            })
+        return result
 
