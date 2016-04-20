28ba865d427e80b17df3d00c2dcd9269f0a85437
shuoh <hanshuo.cmu@gmail.com>
2016-04-20 11:09:24 -0700

    Generate training samples

--- bin/generate_qa_pairs.py
+++ bin/generate_qa_pairs.py
@@ -1,4 +1,5 @@
 import json
+import random
 from qagen.data_provider.knowledge_data_providers import *
 from qagen.qa.collector import KnowledgeDataQaPairCollector
 from qagen.qa.generators import *
@@ -17,14 +18,27 @@ qa_collector.collect_from(data_provider)
 print '%d QA pairs collected.' % len(qa_collector.qa_pairs)
 
 is_for_training = True
+TRAINING_DATA_SAMPLE = 500
+RANKED_ANSWER_COUNT = 5
 
 if is_for_training:
-    print 'Adding additional random answers for rank labeling...'
     output_path = 'qa_pairs_labeled.json'
+    # select random samples
+    output_qa_pairs = random.sample(qa_collector.qa_pairs, TRAINING_DATA_SAMPLE)
+
+    print 'Adding additional random answers for rank labeling...'
+    all_answers = [qa_pair.answer for qa_pair in qa_collector.qa_pairs]
+    for qa_pair in output_qa_pairs:
+        random_answer_sampling = random.sample(all_answers, RANKED_ANSWER_COUNT)
+        for random_answer in random_answer_sampling:
+            qa_pair.add_ranked_answer(random_answer, 20)
+
 else:
     output_path = 'qa_pairs_unlabeled.json'
+    output_qa_pairs = qa_collector.qa_pairs
 
-qa_pairs_json_data = [qa_pair.to_json_dict(is_for_training=is_for_training) for qa_pair in qa_collector.qa_pairs]
+print 'Dumping QA pairs to JSON file...'
+qa_pairs_json_data = [qa_pair.to_json_dict(is_for_training=is_for_training) for qa_pair in output_qa_pairs]
 qa_json_str = json.dumps(qa_pairs_json_data, indent=2, sort_keys=True)
 with open(output_path, 'w') as output_file:
     output_file.write(qa_json_str)
